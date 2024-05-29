bb108c6a5faf665c556e3c42ef29c2d8d4159503
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-05-29 11:32:58 +0700

    using useCallback and memo to prevent re render of a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Counter from './components/Counter/Counter.jsx';
-import Header from './components/Header.jsx';
-import { log } from './log.js';
-import CounterConfigure from './components/Counter/CounterConfigure.jsx';
+import Counter from "./components/Counter/Counter.jsx";
+import Header from "./components/Header.jsx";
+import { log } from "./log.js";
+import CounterConfigure from "./components/Counter/CounterConfigure.jsx";
 
 function App() {
-  log('<App /> rendered');
+  log("<App /> rendered");
 
-  
   const [chosenCount, setChosenCount] = useState(0);
 
-  
-
- function handleSetChange(newCount){
-  setChosenCount(newCount)
- }
+  function handleSetChange(newCount) {
+    setChosenCount(newCount);
+  }
 
   return (
     <>
       <Header />
       <main>
-        <CounterConfigure onSet={handleSetChange}/>
+        <CounterConfigure onSet={handleSetChange} />
         <Counter initialCount={chosenCount} />
       </main>
     </>
